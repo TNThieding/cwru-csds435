5eb24f9ab44cd8dd422519d2a8793bcfb6fee477
Tyler Thieding <tyler@thieding.com>
2021-03-22 23:50:44 -0400

    Finish Ires functions.

--- HW4/decision_tree.py
+++ HW4/decision_tree.py
@@ -8,9 +8,18 @@ Written for CSDS 435: Data Mining (Spring 2021)
 import math
 import sys
 from dataclasses import dataclass
-from typing import Set
+from typing import Set, Tuple
 
 EXIT_CODE_SUCCESS = 0
+
+def log2_zero(x) -> float:
+    """Take base-2 logarithm of x and return 0 if domain error occurs."""
+    try:
+        log_value = math.log2(x)
+    except ValueError:
+        log_value = 0.0
+
+    return log_value
 
 
 @dataclass()
@@ -58,17 +67,64 @@ def entropy(data_set: Set[DataRecord]) -> float:
     proportion_played = len(played) / len(data_set)
     proportion_not_played = len(not_played) / len(data_set)
 
-    try:
-        entropy_value -= proportion_played * math.log(proportion_played, 2)
-    except ValueError:
-        pass  # treat log of zero as zero
-
-    try:
-        entropy_value -= proportion_not_played * math.log(proportion_not_played, 2)
-    except ValueError:
-        pass  # treat log of zero as zero
+    entropy_value -= proportion_played * log2_zero(proportion_played)
+    entropy_value -= proportion_not_played * log2_zero(proportion_not_played)
 
     return entropy_value
+
+
+def _ires_continuous(data_set: Set[DataRecord], attribute_name: str) -> Tuple[float, float]:
+    """Calculate Ires and corresponding split point based on a continuous attribute."""
+    sorted_data = sorted(data_set, key=lambda record: getattr(record, attribute_name))
+
+    smallest_ires = 1.0
+    split_point_for_smallest = 0.0
+
+    # Note: When iterating, don't consider sets for the first and last possible split points since they will contain
+    # sets of one record on one of the sides of the split.
+    for index in range(1, len(sorted_data) - 2):
+        first_value = getattr(sorted_data[index], attribute_name)
+        second_value = getattr(sorted_data[index + 1], attribute_name)
+        candidate_split_point = (first_value + second_value) / 2
+        print(candidate_split_point)
+
+        num_played_below_split = len([
+            record for record in data_set
+            if record.play and getattr(record, attribute_name) <= candidate_split_point
+        ])
+        num_not_played_below_split = len([
+            record for record in data_set
+            if not record.play and getattr(record, attribute_name) <= candidate_split_point
+        ])
+        num_played_above_split = len([
+            record for record in data_set
+            if record.play and getattr(record, attribute_name) > candidate_split_point
+        ])
+        num_not_played_above_split = len([
+            record for record in data_set
+            if not record.play and getattr(record, attribute_name) > candidate_split_point
+        ])
+
+        num_below = num_played_below_split + num_not_played_below_split
+        num_above = num_played_above_split + num_not_played_above_split
+
+        entropy_below = 0.0
+        entropy_below -= (num_played_below_split / num_below) * log2_zero(num_played_below_split / num_below)
+        entropy_below -= (num_not_played_below_split / num_below) * log2_zero(num_not_played_below_split / num_below)
+
+        entropy_above = 0.0
+        entropy_above -= (num_played_above_split / num_above) * log2_zero(num_played_above_split / num_above)
+        entropy_above -= (num_not_played_above_split / num_above) * log2_zero(num_not_played_above_split / num_above)
+
+        candidate_ires = 0.0
+        candidate_ires += (num_below / len(data_set)) * entropy_below
+        candidate_ires += (num_above / len(data_set)) * entropy_above
+
+        if candidate_ires < smallest_ires:
+            smallest_ires = candidate_ires
+            split_point_for_smallest = candidate_split_point
+
+    return smallest_ires, split_point_for_smallest
 
 
 def ires_outlook(data_set: Set[DataRecord]) -> float:
@@ -86,6 +142,31 @@ def ires_outlook(data_set: Set[DataRecord]) -> float:
     rainy = {record for record in data_set if record.outlook == "rainy"}
     proportion_rainy = len(rainy) / len(data_set)
     ires_value += proportion_rainy * entropy(rainy)
+
+    return ires_value
+
+
+def ires_temperature(data_set: Set[DataRecord]) -> Tuple[float, float]:
+    """Calculate Ires and split point based on temperature."""
+    return _ires_continuous(data_set, attribute_name="temperature")
+
+
+def ires_humidity(data_set: Set[DataRecord]) -> Tuple[float, float]:
+    """Calculate Ires and split point based on humidity."""
+    return _ires_continuous(data_set, attribute_name="humidity")
+
+
+def ires_windy(data_set: Set[DataRecord]) -> float:
+    """Calculate Ires based on it's windy."""
+    ires_value = 0.0
+
+    windy = {record for record in data_set if record.windy}
+    proportion_windy = len(windy) / len(data_set)
+    ires_value += proportion_windy * entropy(windy)
+
+    not_windy = {record for record in data_set if not record.windy}
+    proportion_not_windy = len(not_windy) / len(data_set)
+    ires_value += proportion_not_windy * entropy(not_windy)
 
     return ires_value
 
